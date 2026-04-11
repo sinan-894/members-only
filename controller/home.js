271c52bd5f011833a2a6f0205185b73019553816
sinan-894 <mohammedsinan1011@gmail.com>
2026-04-12 03:49:26 +0400

    Create HomePage view

--- controller/home.js
+++ controller/home.js
@@ -1,6 +1,22 @@
 
+const messagesTest = [
+    {
+        author:'messi',
+        date:new Date(),
+        text:'hi this is test message'
+    },
+    {
+        author:'ronaldo',
+        date:new Date(),
+        text:'hi this is also test siuuuuu'
+    }
+]
 function displayHomePage(req,res,next){
-    res.render('homepage')
+    res.render('homepage',{
+        isLogin:false,
+        isMember:false,
+        messages:messagesTest
+    })
 }
 
 
